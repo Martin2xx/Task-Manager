efb2481b294a8483892ea4b5fa1e4fb73eb1369d
Martin <Martin2xx@yahoo.com>
2025-01-12 16:42:02 -0600

    update

--- src/App.js
+++ src/App.js
@@ -5,22 +5,21 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter, Routes, Route } from'react-router-dom';
 import HomePage from './Pages/HomePage/HomePage';
 import ContactUsPage from './Pages/ContactUsPage/ContactUsPage'
+import { HashRouter } from "react-router-dom";
+
 
 function App() {
   return (
     <>
+    <HashRouter>
     <NavBarMenu />
-    <BrowserRouter>
       <Routes>
           <Route path="/home" element={<HomePage />} />
           <Route path="/contactus" element={<ContactUsPage />} />
           <Route path="/" element={<HomePage />} />
         
       </Routes>
-    </BrowserRouter>
-
-
-
+    </HashRouter>
     </>
   )
 }
